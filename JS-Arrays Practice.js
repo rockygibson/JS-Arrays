dfdafbaf51a3ed0a5d10a70972b4ab716bbd12e3
Rocky <rockygibson@gmail.com>
2015-01-17 14:44:04 -0700

    Mentor Session ProgressPost Class 1/17/15

--- JS-Arrays Practice.js
+++ JS-Arrays Practice.js
@@ -73,14 +73,24 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Next problem
 
 
-var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
-var evens = [];
-var odds = []
+var numbers = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var evenNums = [];
+var oddNums = []
 //Write a function called divider that is given three arguments, nums, evens, and odds.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
-
-
+var divider = function(nums, evens, odds){
+  for (var i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    } else {
+      odds.push(nums[i]);
+    }
+  }
+  var result = [evens, odds];
+  return result;
+}
+divider(numbers, evenNums, oddNums);
   
 
 
@@ -91,9 +101,18 @@ var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-//Above your given a function that will return a random number between 0 and 30, and an array full or numbers. Your job is to write a function named finder that will get a random number, then loop through the array to see if that random number is in the array. If it is, return true, if it's not, return false
+//Above your given a function that will return a random number between 0 and 30, and an array full of numbers. Your job is to write a function named finder that will get a random number, then loop through the array to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
+var finder = function(){
+  var randomNum = getRandomArbitrary();
+  for (var i = 0; i < numbers.length; i++) {
+    if (randomNum === numbers[i]) {
+      return true;
+    }
+  }
+  return false;
+};
+finder();
 
 
 //Next problem
